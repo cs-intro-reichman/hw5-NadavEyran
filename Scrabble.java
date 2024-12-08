ae0ce97d7a8617999647963575840789bf632129
NadavEyran <nadav.eyran@post.runi.ac.il>
2024-12-08 16:16:02 +0200

    Q2

--- Scrabble.java
+++ Scrabble.java
@@ -48,25 +48,71 @@ public class Scrabble {
 
 	// Checks if the given word is in the dictionary.
 	public static boolean isWordInDictionary(String word) {
-		//// Replace the following statement with your code
-		return false;
+	    for (int i = 0; i < NUM_OF_WORDS; i++) {
+	        String dictionaryWord = DICTIONARY[i];
+
+	        if (dictionaryWord.length() != word.length()) {
+	            continue;
+	        }
+
+	        boolean isMatch = true;
+	        for (int j = 0; j < word.length(); j++) {
+	            if (dictionaryWord.charAt(j) != word.charAt(j)) {
+	                isMatch = false;
+	                break;
+	            }
+	        }
+
+	        if (isMatch) {
+	            return true;
+	        }
+	    }
+	    return false;
 	}
 	
 	// Returns the Scrabble score of the given word.
 	// If the length of the word equals the length of the hand, adds 50 points to the score.
 	// If the word includes the sequence "runi", adds 1000 points to the game.
 	public static int wordScore(String word) {
-		//// Replace the following statement with your code
-		return 0;
+	    int score = 0;
+
+	    for (int i = 0; i < word.length(); i++) {
+	        score += SCRABBLE_LETTER_VALUES[word.charAt(i) - 'a'];
+	    }
+
+	    if (word.length() == HAND_SIZE) {
+	        score += 50;
+	    }
+
+	    if (word.contains("runi")) {
+	        score += 1000;
+	    }
+
+	    return score;
 	}
+
 
 	// Creates a random hand of length (HAND_SIZE - 2) and then inserts
 	// into it, at random indexes, the letters 'a' and 'e'
 	// (these two vowels make it easier for the user to construct words)
 	public static String createHand() {
-		//// Replace the following statement with your code
-		return null;
+	    StringBuilder hand = new StringBuilder();
+	    int randomHandSize = HAND_SIZE - 2;
+
+	    for (int i = 0; i < randomHandSize; i++) {
+	        char randomChar = (char) ('a' + (int) (Math.random() * 26));
+	        hand.append(randomChar);
+	    }
+
+	    int randomIndexA = (int) (Math.random() * (hand.length() + 1));
+	    int randomIndexE = (int) (Math.random() * (hand.length() + 1));
+
+	    hand.insert(randomIndexA, 'a');
+	    hand.insert(randomIndexE, 'e');
+
+	    return hand.toString();
 	}
+
 	
     // Runs a single hand in a Scrabble game. Each time the user enters a valid word:
     // 1. The letters in the word are removed from the hand, which becomes smaller.
