0ff4580cd26d875503dab0e419953e2cf55f202e
NadavEyran <nadav.eyran@post.runi.ac.il>
2024-12-11 17:13:05 +0200

    Q2 new

--- Scrabble.java
+++ Scrabble.java
@@ -126,22 +126,39 @@ public class Scrabble {
     // 2. The user gets the Scrabble points of the entered word.
     // 3. The user is prompted to enter another word, or '.' to end the hand. 
 	public static void playHand(String hand) {
-		int n = hand.length();
-		int score = 0;
-		// Declares the variable in to refer to an object of type In, and initializes it to represent
-		// the stream of characters coming from the keyboard. Used for reading the user's inputs.   
-		In in = new In();
-		while (hand.length() > 0) {
-			System.out.println("Current Hand: " + MyString.spacedString(hand));
-			System.out.println("Enter a word, or '.' to finish playing this hand:");
-			// Reads the next "token" from the keyboard. A token is defined as a string of 
-			// non-whitespace characters. Whitespace is either space characters, or  
-			// end-of-line characters.
-			String input = in.readString();
-			//// Replace the following break statement with code
-			//// that completes the hand playing loop
-			break;
-		}
+	    int h = hand.length();
+	    int score = 0;
+
+	    // Declares the variable 'in' to refer to an object of type 'In' for reading the user's inputs.
+	    In in = new In();
+
+	    while (hand.length() > 0) {
+	        System.out.println("Current Hand: " + MyString.spacedString(hand));
+	        System.out.println("Enter a word, or '.' to finish playing this hand:");
+
+	        // Reads the next "token" from the keyboard.
+	        String input = in.readString();
+
+	        if (input.equals(".")) {
+	            break;
+	        } else if (!MyString.subsetOf(input, hand)) {
+	            System.out.println("Invalid word. Try again.");
+	        } else if (!isWordInDictionary(input)) {
+	            System.out.println("Not a word. Try again.");
+	        } else if (isWordInDictionary(input)) {
+	            score += wordScore(input);
+	            hand = MyString.remove(hand, input);
+	            System.out.println(input + " earned " + wordScore(input) + " points. Score: " + score + " points\n");
+	        }
+	    }
+
+	    if (hand.length() == 0) {
+	        System.out.println("Ran out of letters. Total score: " + score + " points");
+	    } else {
+	        System.out.println("End of hand. Total score: " + score + " points");
+	    }	
+	}
+
 		if (hand.length() == 0) {
 	        System.out.println("Ran out of letters. Total score: " + score + " points");
 		} else {
